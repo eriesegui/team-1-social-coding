7951aca54727de663bab40b55c2fee71ae68b036
SirChubbx17 <qjccdelacruz@tip.edu.ph>
2022-10-26 01:25:21 +0000

    Added file

---
+++ ip_api.py
@@ -0,0 +1,17 @@
+import urllib.parse
+import requests
+
+main_api = "http://ip-api.com/json/"
+
+json_data = requests.get(main_api).json()
+
+print("Public IP Address:   " + str(json_data["query"]))
+print("==================================================\nInformation")
+print("Country:     " + json_data["country"])
+print("Country Code:" + json_data["countryCode"])
+print("Region:      " + json_data["regionName"])
+print("City:        " + json_data["city"])
+print("Zip Code:    " + str(json_data["zip"]))
+print("ISP:         " + json_data["isp"])
+print("Timezone:    " + json_data["timezone"])
+print("ASN:         " + json_data["as"])
