ddefa72f1d1f80fe32f61bc3c6ce705da163563c
SirChubbx17 <qjccdelacruz@tip.edu.ph>
2022-11-02 01:55:41 +0000

    project activity 4

--- ip_api.py
+++ ip_api.py
@@ -1,11 +1,25 @@
 import urllib.parse
 import requests
 
-main_api = "http://ip-api.com/json/"
+file = open('logo.txt', 'r')
+logo = file.read()
+print(logo)
+file.close()
+
+ipv4_api = "http://ip-api.com/json/"
+
+your_ipv4 = requests.get(ipv4_api).json()
+
+print("\nYour Public IPv4 Address: " + str(your_ipv4["query"]))
+
+print("\nQuery an IP Address. Press ENTER to query own IP.")
+query = input("Enter IP Address: ")
+
+main_api = "http://ip-api.com/json/"+query
 
 json_data = requests.get(main_api).json()
 
-print("Public IP Address:   " + str(json_data["query"]))
+print("\nPublic IP Address:   " + str(json_data["query"]))
 print("==================================================\nInformation")
 print("Country:     " + json_data["country"])
 print("Country Code:" + json_data["countryCode"])
